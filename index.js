15c27800cf0f3a8c5697821663870070cd254d81
Thomas <themagnetiiktbk@gmail.com>
2023-02-23 10:38:37 +0100

    Update execSync in index.js to fix the 'missing playbook parameter'

--- index.js
+++ index.js
@@ -114,7 +114,7 @@ function deploy_site(site_name, site, site_env){
 
 function run_playbook(site_name, site_env, sha) {
     try {
-        const child = child_process.execSync('ansible-playbook', ['deploy.yml',`-e site=${site_name}`, `-e env=${site_env}`, `-e site_version=${sha} ${verbose}`]);
+        const child = child_process.execSync(`ansible-playbook deploy.yml -e site=${site_name} -e env=${site_env} -e site_version=${sha} ${verbose}`);
 
         if( child.stdout )
             console.log(`${child.stdout}`);
